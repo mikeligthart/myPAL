3851a94873f0d3a7d44ab941e6552039379f285e
Mike Ligthart <mike.ligthart@gmail.com>
2015-05-29 12:57:43 +0200

    added greeting animation of avatar to greeting page with text

--- myPAL/app/controllers/Application.java
+++ myPAL/app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(greeting.render());
+        return ok(greeting.render("Jelte"));
     }
 
     public static Result showBootstrap() { return ok(bootstrap.render("Hello World!"));}

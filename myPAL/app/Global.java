c4d6f3cf7e6611d3be2d063a2c78e10ab3817f4d
Mike Ligthart <mike.ligthart@gmail.com>
2015-08-21 14:26:02 +0200

    Migrated from in memory H2 database to a persistant one

--- myPAL/app/Global.java
+++ myPAL/app/Global.java
@@ -20,6 +20,7 @@ import static play.mvc.Results.*;
 public class Global extends GlobalSettings {
     //https://github.com/jamesward/zentasks/blob/master/app/Global.java
 
+    /*
     public void onStart(Application app) {
         InitialData.insert(app);
     }
@@ -33,20 +34,9 @@ public class Global extends GlobalSettings {
 
                 // Insert users first
                 Ebean.save(all.get("users"));
-
-                // Insert projects
-                //Ebean.save(all.get("emotion"));
-                /*
-                for(Object project: all.get("projects")) {
-                    // Insert the project/user relation
-                    Ebean.saveManyToManyAssociations(project, "members");
-                }
-                */
-
-                // Insert tasks
-                //Ebean.save(all.get("diary_activity"));
             }
         }
 
     }
+    */
 }

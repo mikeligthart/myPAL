e94576c9b7c162a60bf3c198abbc0d79153c0f53
Mike Ligthart <mike.ligthart@gmail.com>
2015-08-28 20:21:02 +0200

    added test admin

--- myPAL/app/Global.java
+++ myPAL/app/Global.java
@@ -22,6 +22,8 @@ public class Global extends GlobalSettings {
             if(Ebean.find(User.class).findRowCount() == 0) {
                 User originalUser = new User("mike.ligthart@gmail.com", "Mike", "Ligthart", Date.valueOf(LocalDate.now()), "mike", UserType.ADMIN);
                 Ebean.save(originalUser);
+                User testAdmin = new User("test@email.com", "Test", "Admin", Date.valueOf(LocalDate.now()), "secret", UserType.ADMIN);
+                Ebean.save(testAdmin);
             }
         }
 
